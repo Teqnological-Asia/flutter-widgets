5e1ed33722ee984a057703c6498fcac2d3ecb4ef
Nguyen Anh Tuan <tuanura2010@gmail.com>
2023-10-25 15:48:11 +0700

    [TEP-1172] Change name

--- packages/syncfusion_flutter_xlsio/lib/src/xlsio/general/enums.dart
+++ packages/syncfusion_flutter_xlsio/lib/src/xlsio/general/enums.dart
@@ -1444,7 +1444,7 @@ enum ActivePane {
 }
 
 /// Enum defining different editing modes for an object within a cell-based system.
-enum EditAs {
+enum PictureResizeMode {
   /// In this mode, when you resize the cell, the Image remains the same size,
   /// and only the cell's dimensions are adjusted.
   moveButDontSizeWithCell('oneCell'),
@@ -1457,5 +1457,5 @@ enum EditAs {
   final String value;
 
   /// Constructor to initialize the enum with a string value.
-  const EditAs(this.value);
+  const PictureResizeMode(this.value);
 }
